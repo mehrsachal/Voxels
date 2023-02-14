a4bc6786d1c4dddc90be7567d952b1f1bf7da7c6
Mehr Sachal <mehrsachal@gmail.com>
2023-02-15 00:27:17 +0500

    updated to count open faces by the voxel

--- main.py
+++ main.py
@@ -3,11 +3,19 @@ import visualiser
 import blockvis
 import optblockvis
 import openfaces
+import listopenfaces
 
 pyramidgen.genpyr(2)
-pr=visualiser.plot_voxel()
-pt = openfaces.list_voxel_faces(pr)
-print(len(pt))
+voxel_array=visualiser.plot_voxel()
+# open_faces = openfaces.list_voxel_faces(voxel_array)
+# print(len(open_faces))
+# print(open_faces)
+
+faces, open_face_count =listopenfaces.list_voxel_faces(voxel_array)
+
+print("Faces:", faces)
+print("Open face count:", open_face_count)
+
 
 #blockvis.plot_blocks()
 #optblockvis.plot_blocks()
