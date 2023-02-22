14570f7ba43fb971c1f7793e5532e4d075647ec8
Mehr Sachal <mehrsachal@gmail.com>
2023-02-22 16:12:55 +0500

    latest

--- blockopenfaces.py
+++ blockopenfaces.py
@@ -1,30 +1,103 @@
 import numpy as np
 
-def list_voxel_faces(voxel_array):
+def get_voxel_faces(voxels):
     """
-    Given an array of voxels, return a list of all the faces of the voxels
-    and a dictionary that maps each voxel to the number of open faces,
-    excluding the bottom face of voxels in the bottom.
-    The voxel_array should have shape (n, 6) where n is the number of voxels
-    and 6 represents the x, y, z coordinates and size of each voxel.
+    Returns a dictionary containing the faces of each voxel.
+
+    Parameters:
+    voxels (np.ndarray): numpy array of shape (n, 6) representing the voxels
+
+    Returns:
+    dict: a dictionary where the keys are the indices of the voxels and the
+    values are lists of tuples representing the faces of the voxel, where
+    each tuple contains the x, y, z coordinates of the face and the normal vector
+    of the face (in the form of a tuple of three integers, where each integer
+    represents the direction of the normal vector in the x, y, and z directions).
     """
-    voxel_set = set(tuple(v) for v in voxel_array)
-    faces = []
-    open_face_count = {}
-    for voxel in voxel_set:
-        x, y, z, s, d, f = voxel
-        open_faces = 0
-        for i in range(-s, s+1):
-            if (x+i, y, z) not in voxel_set:
-                faces.append([(x+i, y, z), (x+i, y+d, z), (x+i, y+d, z+f), (x+i, y, z+f)])
-                open_faces += 1
-        for j in range(-d, d+1):
-            if (x, y+j, z) not in voxel_set:
-                faces.append([(x, y+j, z), (x+s, y+j, z), (x+s, y+j, z+f), (x, y+j, z+f)])
-                open_faces += 1
-        for k in range(-f, f+1):
-            if (x, y, z+k) not in voxel_set and z+k >= 0:
-                faces.append([(x, y, z+k), (x+s, y, z+k), (x+s, y+d, z+k), (x, y+d, z+k)])
-                open_faces += 1
-        open_face_count[voxel] = open_faces
-    return faces, open_face_count
+
+    voxel_faces = {}
+
+    for i, voxel in enumerate(voxels):
+        x, y, z, size_x, size_y, size_z = voxel
+        faces = []
+
+        # Front face
+        faces.append(((x, y, z), (0, 0, 1)))
+        faces.append(((x + size_x, y, z), (0, 0, 1)))
+        faces.append(((x + size_x, y + size_y, z), (0, 0, 1)))
+        faces.append(((x, y + size_y, z), (0, 0, 1)))
+
+        # Back face
+        faces.append(((x, y, z + size_z), (0, 0, -1)))
+        faces.append(((x + size_x, y, z + size_z), (0, 0, -1)))
+        faces.append(((x + size_x, y + size_y, z + size_z), (0, 0, -1)))
+        faces.append(((x, y + size_y, z + size_z), (0, 0, -1)))
+
+        # Top face
+        faces.append(((x, y + size_y, z), (0, 1, 0)))
+        faces.append(((x + size_x, y + size_y, z), (0, 1, 0)))
+        faces.append(((x + size_x, y + size_y, z + size_z), (0, 1, 0)))
+        faces.append(((x, y + size_y, z + size_z), (0, 1, 0)))
+
+        # Bottom face
+        faces.append(((x, y, z), (0, -1, 0)))
+        faces.append(((x + size_x, y, z), (0, -1, 0)))
+        faces.append(((x + size_x, y, z + size_z), (0, -1, 0)))
+        faces.append(((x, y, z + size_z), (0, -1, 0)))
+
+        # Left face
+        faces.append(((x, y, z), (-1, 0, 0)))
+        faces.append(((x, y, z + size_z), (-1, 0, 0)))
+        faces.append(((x, y + size_y, z + size_z), (-1, 0, 0)))
+        faces.append(((x, y + size_y, z), (-1, 0, 0)))
+
+                # Right face
+        faces.append(((x + size_x, y, z), (1, 0, 0)))
+        faces.append(((x + size_x, y, z + size_z), (1, 0, 0)))
+        faces.append(((x + size_x, y + size_y, z + size_z), (1, 0, 0)))
+        faces.append(((x + size_x, y + size_y, z), (1, 0, 0)))
+
+        voxel_faces[i] = faces
+
+    return voxel_faces
+
+def count_shared_faces(faces, voxel_array):
+    """
+    Counts the number of shared faces between neighboring voxels.
+
+    Parameters:
+    faces (dict): a dictionary where the keys are the indices of the voxels
+    and the values are lists of tuples representing the faces of the corresponding voxel.
+    voxel_array (np.ndarray): numpy array of shape (n, 6) representing the voxels
+
+    Returns:
+    dict: a dictionary where the keys are tuples representing pairs of neighboring
+    voxels, and the values are integers representing the number of shared faces
+    between the two voxels.
+    """
+
+    shared_faces = {}
+
+    for i, voxel_faces_i in faces.items():
+        x_i, y_i, z_i, size_x_i, size_y_i, size_z_i = voxel_array[i]
+        for j in range(i+1, len(faces)):
+            voxel_faces_j = faces[j]
+            x_j, y_j, z_j, size_x_j, size_y_j, size_z_j = voxel_array[j]
+            shared = 0
+            for face_i in voxel_faces_i:
+                for face_j in voxel_faces_j:
+                    if face_i == face_j:
+                        # Check if the faces are adjacent
+                        x_diff = x_i - x_j
+                        y_diff = y_i - y_j
+                        z_diff = z_i - z_j
+                        if abs(x_diff) == size_x_i and abs(y_diff) < size_y_i and abs(z_diff) < size_z_i:
+                            shared += 1
+                        elif abs(y_diff) == size_y_i and abs(x_diff) < size_x_i and abs(z_diff) < size_z_i:
+                            shared += 1
+                        elif abs(z_diff) == size_z_i and abs(x_diff) < size_x_i and abs(y_diff) < size_y_i:
+                            shared += 1
+            if shared > 0:
+                shared_faces[(i, j)] = shared
+
+    return shared_faces
--- main.py
+++ main.py
@@ -13,14 +13,15 @@ import blockopenfaces
 # print (len(faces))
 
 voxel_array=blockvis.plot_blocks()
-faces, openfacecount = blockopenfaces.list_voxel_faces(voxel_array)
-print (len(faces))
 
+faces = blockopenfaces.get_voxel_faces(voxel_array)
 
+# Count the number of shared faces between neighboring voxels
+shared_faces = blockopenfaces.count_shared_faces(faces, voxel_array)
 
-# nextbatch = utilityvox.remove_voxel(voxel_array, n_on_top)
+# Count the total number of faces
+total_faces = sum([len(faces_i) for faces_i in faces.values()]) / 2
+for shared in shared_faces.values():
+    total_faces += shared
 
-
-
-        
-        
+print(total_faces)
